ff169ad83f0b921f146281b20e64cd9078c2e714
rui.xue <354221173@qq.com>
2019-01-22 11:58:25 +0800

    xxx

--- resource/JavaScript/product.js
+++ resource/JavaScript/product.js
@@ -17,10 +17,10 @@ if(titleObj.num==1){
     $(document).scrollTop(1855)
     $('.sidebar a').eq(2).delay(300).addClass("active").siblings("a").removeClass("active");
 }else if(titleObj.num==4){
-    $(document).scrollTop(2784)
+    $(document).scrollTop(3567)
     $('.sidebar a').eq(3).delay(300).addClass("active").siblings("a").removeClass("active");
 }else if(titleObj.num==5){
-    $(document).scrollTop(3311)
+    $(document).scrollTop(4028)
     $('.sidebar a').eq(4).delay(300).addClass("active").siblings("a").removeClass("active");
 }else{
     $('.sidebar a').eq(0).delay(300).addClass("active").siblings("a").removeClass("active");
@@ -30,11 +30,11 @@ $(window).scroll(function () {
         $('.sidebar a').eq(0).delay(300).addClass("active").siblings("a").removeClass("active");
     }else if($(document).scrollTop()>953&&$(document).scrollTop()<1855){
         $('.sidebar a').eq(1).delay(300).addClass("active").siblings("a").removeClass("active");
-    }else if($(document).scrollTop()>1855&&$(document).scrollTop()<2784){
+    }else if($(document).scrollTop()>1855&&$(document).scrollTop()<3567){
         $('.sidebar a').eq(2).delay(300).addClass("active").siblings("a").removeClass("active");
-    }else if($(document).scrollTop()>2784&&$(document).scrollTop()<3311){
+    }else if($(document).scrollTop()>3567&&$(document).scrollTop()<4028){
         $('.sidebar a').eq(3).delay(300).addClass("active").siblings("a").removeClass("active");
-    }else if($(document).scrollTop()>3311){
+    }else if($(document).scrollTop()>4028){
         $('.sidebar a').eq(4).delay(300).addClass("active").siblings("a").removeClass("active");
     }
 })
